53875a422f2635c90e6df9ca43df06b905c50f95
01bit <1545295762@qq.com>
2024-07-10 13:32:50 +0800

    优化颜色存储方式

--- 0MLS1/scoretable/color_parser.py
+++ 0MLS1/scoretable/color_parser.py
@@ -1,24 +1,37 @@
 from bs4 import BeautifulSoup
 import json
+marbles = {}
+hexes = []
+
 with open('color_table.html', 'r', encoding='utf-8') as fp:
     content = fp.read()
     soup = BeautifulSoup(content, 'lxml')
-table = soup.find('table', {'class': 'colortable'})
-tbody = table.tbody
+tables = soup.find_all('table', {'class': 'colortable'})
+
+tbody = tables[1].tbody
 trs = tbody.find_all('tr')
-marbles = []
-hexes = []
 for tr in trs:
     tds = tr.find_all('td')
-    marble = {}
     if len(tds) == 0:
         continue
-    marble['id'] = tds[2].dfn.string.strip()
-    marble['name'] = marble['id'].capitalize()
+    id = tds[2].dfn.string.strip()
+    marble = {}
+    marble['name'] = id.capitalize()
     marble['hex'] = tds[3].string.strip()
     marble['color'] = [int(i) for i in tds[4].string.strip().split(',')]
+    marble['basic'] = False
     if marble['hex'] not in hexes:
-        marbles.append(marble)
+        marbles[id] = marble
         hexes.append(marble['hex'])
+
+tbody = tables[0].tbody
+trs = tbody.find_all('tr')
+for tr in trs:
+    tds = tr.find_all('td')
+    if len(tds) == 0:
+        continue
+    id = tds[2].dfn.string.strip()
+    marbles[id]['basic'] = True
+
 with open('marble_list.json', 'w', encoding='utf-8') as fp:
     fp.write(json.dumps(marbles, ensure_ascii=False))
